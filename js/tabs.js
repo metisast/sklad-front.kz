d717d485ab03dd1a26f06bbcadabd163e687144d
EugeneKu <metis_ast@mail.ru>
2016-06-22 23:01:47 +0600

    Filter start

--- js/tabs.js
+++ js/tabs.js
@@ -4,7 +4,7 @@ $(function() {
 	$('ul.tabs__caption').on('click', 'li:not(.active)', function() {
 		$(this)
 			.addClass('active').siblings().removeClass('active')
-			.closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+			.closest('div#filter').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
 	});
 
 });
